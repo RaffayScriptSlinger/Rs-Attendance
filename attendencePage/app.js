a8476a435913671a02bdf96c329c9d77ee420950
RaffayScriptSlinger <raffaysharjeel@gmail.com>
2024-07-17 01:18:15 -0930

    Some minor Changes in Attendence Page

--- attendencePage/app.js
+++ attendencePage/app.js
@@ -2,32 +2,35 @@ let attendanceList = [];
 
 function markBtn() {
    let userRollNumber = document.getElementById('rollNumber').value;
+   let userName = document.getElementById('Name').value;
 
-   if (userRollNumber) {
+   if (userRollNumber && userName) {
       Swal.fire("Congratulations! Your attendance is marked.");
 
-     
+
 
       const studentAttendance = {
          rollNumber: userRollNumber,
          time: new Date().toLocaleString(),
+         Name: userName,
 
       };
       attendanceList.push(studentAttendance);
 
-      console.log(attendanceList);
+      // console.log(attendanceList);
 
-      Swal.fire(`${userRollNumber} attendance marked at ${new Date()}`);
+      Swal.fire(` ${userName} having a RollNumber ${userRollNumber} attendance marked at ${new Date()}`);
    } else {
-      Swal.fire("Kindly fill in your Roll Number");
+      Swal.fire("RollNumber or Name Is Missing!");
    }
    document.getElementById('rollNumber').value = "";
+   document.getElementById('Name').value = "";
 
 }
 
 function downloadExcel() {
-    const ws = XLSX.utils.json_to_sheet(attendanceList);
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, "Attendance");
-    XLSX.writeFile(wb, "attendance.xlsx");
- }
+   const ws = XLSX.utils.json_to_sheet(attendanceList);
+   const wb = XLSX.utils.book_new();
+   XLSX.utils.book_append_sheet(wb, ws, "Attendance");
+   XLSX.writeFile(wb, "attendance.xlsx");
+}
